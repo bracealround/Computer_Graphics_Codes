3c03d90743ee77b54e6b2526f37b974c1f9de1ca
Sourav Saha <40169851+bracealround@users.noreply.github.com>
2021-10-28 18:21:27 +0600

    Add files via upload

---
+++ Circle_Drawing_Algo/midpoint_circle.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+#include<windows.h>
+
+
+using namespace std;
+
+const int win_h=600,win_w=800;
+
+///Structure of a point
+struct PT{
+
+    int x,y;
+    PT(){}
+    PT(double a,double b)
+    {
+        x=a;
+        y=b;
+    }
+
+};
+
+///This method converts the coordinate system from a traditional one to a computer recognized one.
+PT convert_pixel(PT p)
+{
+    p.x+=(double)(win_w/2);
+    p.y= p.y*(-1.00);
+    p.y+=(double)(win_h/2);
+
+    return p;
+}
+
+///This method just draws the desired pixel.
+void draw_pixel(PT p,int color)
+{
+    //for drawing pixel
+    delay(1);
+    p=convert_pixel(p);
+    putpixel((int)(p.x+0.5),(int)(p.y+0.5),color);
+}
+
+///This method draws the traditional axis lines on the window.
+void draw_axis(int x,int y)
+{
+    //for drawing x axis
+    for(int i=0;i<x;i++)
+        putpixel(i,(y/2),WHITE);
+
+    //for drawing y axis
+    for(int i=0;i<y;i++)
+        putpixel((x/2),i,WHITE);
+
+    return ;
+}
+
+///This function draws all the mirror points to form a circle
+void mirror_point_draw(PT center, PT a)
+{
+    draw_pixel(PT(a.x + center.x, a.y + center.y),YELLOW);
+    draw_pixel(PT(-a.x + center.x, a.y + center.y),YELLOW);
+    draw_pixel(PT(a.x + center.x, -a.y + center.y),YELLOW);
+    draw_pixel(PT(-a.x + center.x, -a.y + center.y),YELLOW);
+    draw_pixel(PT(a.y + center.x, a.x + center.y),YELLOW);
+    draw_pixel(PT(-a.y + center.x, a.x + center.y),YELLOW);
+    draw_pixel(PT(a.y + center.x, -a.x + center.y),YELLOW);
+    draw_pixel(PT(-a.y + center.x, -a.x + center.y),YELLOW);
+
+}
+
+///Midpoint circle drawing algorithm function
+void mid_point_circle(PT center, int radious)
+{
+    PT a;
+    a.x = 0;
+    a.y = radious;
+
+    int decision_parameter = 1-radious; //Although the equation is 1.25-r but for practical purpose it should suffice.
+
+    while(a.x<=a.y)
+    {
+        mirror_point_draw(center, a);
+
+        a.x++;
+
+        if(decision_parameter<0)
+            decision_parameter = decision_parameter + (2 * a.x) + 3;
+        else
+        {
+            a.y--;
+            decision_parameter = decision_parameter + (2 * a.x) - (2 * a.y) + 5;
+        }
+
+    }
+}
+
+
+int main()
+{
+    initwindow(win_w,win_h);
+    //For drawing axis
+    draw_axis(win_w,win_h);
+
+    //mid_point_circle(PT(50,50),100);
+    mid_point_circle(PT(0,0),100);
+    getchar();
+
+    return 0;
+}
